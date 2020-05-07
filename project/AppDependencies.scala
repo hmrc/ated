67a0396518316675a311e5745d9e40e72a8aad12
jmountain-hmrc <31282623+jmountain-hmrc@users.noreply.github.com>
2020-05-07 12:28:54 +0100

    DL-2863 Uplifted ATED to Scala 2.12 and SBT 1.3

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,6 @@
 import sbt._
 
-object MicroServiceBuild extends Build with MicroService {
-  val appName = "ated"
-
-  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
-}
-
-private object AppDependencies {
+object AppDependencies {
 
   import play.sbt.PlayImport._
   import play.core.PlayVersion
@@ -14,12 +8,12 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
 		"com.enragedginger" %% "akka-quartz-scheduler" % "1.8.0-akka-2.5.x",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"     % "1.4.0",
-    "uk.gov.hmrc"       %% "domain"                % "5.6.0-play-26",
-    "uk.gov.hmrc"       %% "simple-reactivemongo"  % "7.23.0-play-26",
-    "uk.gov.hmrc"       %% "json-encryption"       % "4.4.0-play-26",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"     % "1.7.0",
+    "uk.gov.hmrc"       %% "domain"                % "5.8.0-play-26",
+    "uk.gov.hmrc"       %% "simple-reactivemongo"  % "7.26.0-play-26",
+    "uk.gov.hmrc"       %% "json-encryption"       % "4.5.0-play-26",
     "uk.gov.hmrc"       %% "auth-client"           % "2.33.0-play-26",
-		"uk.gov.hmrc"			  %% "mongo-lock"					   % "6.18.0-play-26",
+		"uk.gov.hmrc"			  %% "mongo-lock"					   % "6.21.0-play-26",
 		"com.typesafe.play" %% "play-json-joda"        % "2.6.10"
   )
 
@@ -29,14 +23,14 @@ private object AppDependencies {
   }
 
   object Test {
-    def apply() = new TestDependencies {
+    def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
         "org.scalatest"           %% "scalatest"          % "3.0.5"             % scope,
         "org.scalatestplus.play"  %% "scalatestplus-play" % "3.1.2"             % scope,
         "org.pegdown"              % "pegdown"            % "1.6.0"             % scope,
         "org.mockito"              % "mockito-core"       % "3.0.0"            % scope,
         "com.typesafe.play"       %% "play-test"          % PlayVersion.current % scope,
-        "uk.gov.hmrc"             %% "reactivemongo-test" % "4.16.0-play-26"    % scope
+        "uk.gov.hmrc"             %% "reactivemongo-test" % "4.19.0-play-26"    % scope
       )
     }.test
   }
