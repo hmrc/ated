fe04616a054cfb1c9ad06e765570d93341eee9a8
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2020-05-20 10:47:59 +0100

    DL-3236 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,11 +8,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
 		"com.enragedginger" %% "akka-quartz-scheduler" % "1.8.0-akka-2.5.x",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"     % "1.7.0",
-    "uk.gov.hmrc"       %% "domain"                % "5.8.0-play-26",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"     % "1.8.0",
+    "uk.gov.hmrc"       %% "domain"                % "5.9.0-play-26",
     "uk.gov.hmrc"       %% "simple-reactivemongo"  % "7.26.0-play-26",
     "uk.gov.hmrc"       %% "json-encryption"       % "4.5.0-play-26",
-    "uk.gov.hmrc"       %% "auth-client"           % "2.33.0-play-26",
+    "uk.gov.hmrc"       %% "auth-client"           % "3.0.0-play-26",
 		"uk.gov.hmrc"			  %% "mongo-lock"					   % "6.21.0-play-26",
 		"com.typesafe.play" %% "play-json-joda"        % "2.6.10"
   )
@@ -25,10 +25,10 @@ object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
-        "org.scalatest"           %% "scalatest"          % "3.0.5"             % scope,
-        "org.scalatestplus.play"  %% "scalatestplus-play" % "3.1.2"             % scope,
+        "org.scalatest"           %% "scalatest"          % "3.0.8"             % scope,
+        "org.scalatestplus.play"  %% "scalatestplus-play" % "3.1.3"             % scope,
         "org.pegdown"              % "pegdown"            % "1.6.0"             % scope,
-        "org.mockito"              % "mockito-core"       % "3.0.0"            % scope,
+        "org.mockito"              % "mockito-core"       % "3.3.3"            % scope,
         "com.typesafe.play"       %% "play-test"          % PlayVersion.current % scope,
         "uk.gov.hmrc"             %% "reactivemongo-test" % "4.19.0-play-26"    % scope
       )
@@ -41,7 +41,7 @@ object AppDependencies {
 				"uk.gov.hmrc"						 %% "hmrctest" 						 % 	"3.9.0-play-26" 				% scope,
 				"org.pegdown" 					 % "pegdown" 							 % 	"1.6.0"								  % scope,
 				"com.typesafe.play"			 %% "play-test" 				 	 % 	PlayVersion.current 		% scope,
-				"org.scalatestplus.play" %% "scalatestplus-play"	 % 	"3.1.2" 								% scope,
+				"org.scalatestplus.play" %% "scalatestplus-play"	 % 	"3.1.3" 								% scope,
 				"com.github.tomakehurst" % "wiremock-jre8"				 % 	"2.23.2" 								% scope
 			)
 		}.test
