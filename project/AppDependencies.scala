db8e10bb4f6de9a5c89d8c621a0b6d1aada201e1
jayasreemca <7058921+jayasreemca@users.noreply.github.com>
2026-09-10 11:53:16 +0100

    DL-20195: Update libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,22 @@ import sbt.*
 
 private object AppDependencies {
 
-  val bootstrapVersion = "10.7.0"
+  val bootstrapVersion = "10.8.0"
+  val pekkoVersion     = "1.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.2-pekko-1.0.x",
-    "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % "2.13.0",
-    "uk.gov.hmrc"            %% "crypto-json-play-30"       % "8.4.0"
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-30"   % bootstrapVersion,
+    "io.github.samueleresca" %% "pekko-quartz-scheduler"      % "1.3.0-pekko-1.1.x"  exclude("com.mchange", "mchange-commons-java"),
+    "com.mchange"            %  "mchange-commons-java"        % "0.6.1",
+    "uk.gov.hmrc"            %% "domain-play-30"              % "13.0.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"          % "2.13.0",
+    "uk.gov.hmrc"            %% "crypto-json-play-30"         % "8.4.0",
+    "org.apache.pekko"       %% "pekko-protobuf-v3"           % pekkoVersion,
+    "org.apache.pekko"       %% "pekko-serialization-jackson" % pekkoVersion,
+    "org.apache.pekko"       %% "pekko-stream"                % pekkoVersion,
+    "org.apache.pekko"       %% "pekko-actor-typed"           % pekkoVersion,
+    "org.apache.pekko"       %% "pekko-slf4j"                 % pekkoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
