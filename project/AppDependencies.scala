145178a1d1a5329ec9920320ed45cbc19c9fd17f
jayasreemca <7058921+jayasreemca@users.noreply.github.com>
2026-09-14 13:27:34 +0100

    DL-20195: Changing pekko version to 1.2.2-pekko-1.0.x

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ import sbt.*
 private object AppDependencies {
 
   val bootstrapVersion = "10.8.0"
-  val pekkoVersion     = "1.7.0"
+  val pekkoVersion     = "1.1.2"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"   % bootstrapVersion,
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"      % "1.3.0-pekko-1.1.x" exclude("com.mchange", "c3p0") exclude("com.mchange", "mchange-commons-java"),
+    "io.github.samueleresca" %% "pekko-quartz-scheduler"      % "1.2.2-pekko-1.0.x" exclude("com.mchange", "c3p0") exclude("com.mchange", "mchange-commons-java"),
     "com.mchange"            %  "c3p0"                        % "0.14.2",
     "com.mchange"            %  "mchange-commons-java"        % "0.6.1",
     "uk.gov.hmrc"            %% "domain-play-30"              % "13.0.0",
