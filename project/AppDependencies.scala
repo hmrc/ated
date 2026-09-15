d927955d89c3639aa4db3d0c00d147609a760030
Jayasree Atmakuru <7058921+jayasreemca@users.noreply.github.com>
2026-09-15 09:50:23 +0100

    DL-20195: Address CVE-2026-27727 (#109)
    * DL-20195: Update libraries to 1.2.2-pekko-1.0.x.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,17 @@ import sbt.*
 
 private object AppDependencies {
 
-  val bootstrapVersion = "10.7.0"
+  val bootstrapVersion = "10.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.2-pekko-1.0.x",
-    "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % "2.13.0",
-    "uk.gov.hmrc"            %% "crypto-json-play-30"       % "8.4.0"
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-30"   % bootstrapVersion,
+    "io.github.samueleresca" %% "pekko-quartz-scheduler"      % "1.2.2-pekko-1.0.x" exclude("com.mchange", "c3p0") exclude("com.mchange", "mchange-commons-java"),
+    "com.mchange"            %  "c3p0"                        % "0.14.2",
+    "com.mchange"            %  "mchange-commons-java"        % "0.6.1",
+    "uk.gov.hmrc"            %% "domain-play-30"              % "13.0.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"          % "2.13.0",
+    "uk.gov.hmrc"            %% "crypto-json-play-30"         % "8.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
